c0f8e04ef576cb2ba09d0a9262becc0cec5cf1d2
Chharng_Chhit <chhit085@gmail.com>
2024-03-29 14:14:12 +0700

    Gradle Springboot

---
+++ src/test/java/i4/GIC/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package i4.GIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
